1f261178de2eb3a2ea0df6965949f06c0a733424
Vadim Kozin <ulin.salma@yandex.ru>
2021-07-20 10:01:01 +0300

    fix problem in form-edit

--- client/src/components/customer/forms/form-edit.js
+++ client/src/components/customer/forms/form-edit.js
@@ -6,19 +6,18 @@ import React, {
   useContext,
 } from 'react'
 import { useParams } from 'react-router-dom'
-import { fetchCustomers } from 'src/store/api-action'
-import FormCustomerMain from 'src/components/customer/forms/form-customer-main'
-import FormPersonalMain from 'src/components/customer/forms/form-personal-main'
 import ShowError from 'src/common/show-error'
 import ShowProgress from 'src/common/show-progress'
-import { AppContext, ctx } from 'src/context/app-context'
+import { fetchCustomers } from 'src/store/api-action'
+import { MainContext } from 'src/context/main-context'
+import FormCustomerMain from 'src/components/customer/forms/form-customer-main'
+import FormPersonalMain from 'src/components/customer/forms/form-personal-main'
 
 const getCustomer = (customers, custId) =>
   customers.find((cust) => String(cust.custId) === String(custId))
 
 const FormEdit = () => {
-  // eslint-disable-next-line
-  const [contextApp, setContextApp] = useContext(AppContext)
+  const main = useContext(MainContext)
   const { cid } = useParams()
   const [customer, setCustomer] = useState(null)
   const [error, setError] = useState(null)
@@ -26,20 +25,20 @@ const FormEdit = () => {
 
   const fetchData = useCallback(async () => {
     try {
-      if (ctx.isCustomers(contextApp)) {
-        setCustomer(getCustomer(contextApp.customers, cid))
+      if (main.isCustomers()) {
+        setCustomer(getCustomer(main.customers, cid))
       } else {
         const customers = await fetchCustomers()
         if (!mountedRef.current) return null
         const customer = getCustomer(customers, cid)
         if (!customer) throw new Error(`Клиент с кодом : ${cid} не найден.`)
         setCustomer(getCustomer(customers, cid))
-        setContextApp((context) => ({ ...context, customers }))
+        main.saveCustomers(customers)
       }
     } catch (error) {
       setError(error)
     }
-  }, [cid, contextApp, setContextApp])
+  }, [cid, main])
 
   useEffect(() => {
     fetchData()
@@ -49,20 +48,10 @@ const FormEdit = () => {
   const go = (custType) => {
     switch (custType) {
       case 'u':
-        return (
-          <FormCustomerMain
-            isNewCustomer={false}
-            customer={customer}
-          />
-        )
+        return <FormCustomerMain isNewCustomer={false} customer={customer} />
 
       case 'f':
-        return (
-          <FormPersonalMain
-            isNewCustomer={false}
-            customer={customer}
-          />
-        )
+        return <FormPersonalMain isNewCustomer={false} customer={customer} />
 
       default:
         return (
