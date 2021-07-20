e4e55e9496498d0a7f00eaaab02ed85b6f1e3209
Vadim Kozin <ulin.salma@yandex.ru>
2021-07-20 09:46:31 +0300

    refactoring auth/login on server-side

--- routes/auth.js
+++ routes/auth.js
@@ -10,15 +10,12 @@ const { log, TableDb } = require('./helper')
 // (POST) /api/auth/login
 authRouter.route('/login').post(async (req, res) => {
   const { login, password } = req.body
-  console.log(`auth:`, login, password)
   try {
     const user = await db
       .first('id', 'pass')
       .from(TableDb.USERS)
       .where('user', '=', login)
       .on('query', (data) => log(data))
-
-    console.log(`user:`, user)
 
     if (!user) {
       return res.status(400).json({ message: 'Пользователь не найден', ecode:'ERR_USER' })
@@ -37,11 +34,7 @@ authRouter.route('/login').post(async (req, res) => {
       expiresIn: '1h',
     })
 
-    // res.status(200).json(user)
-    const dispath = { token, userId: user.id, userName: login }
-    console.log(`server_send:`, dispath)
-    // res.status(200).json({ token, userId: user.id, userName: login })
-    res.status(200).json(dispath)
+    res.status(200).json({ token, userId: user.id, userName: login })
 
   } catch (e) {
     errorHandler(res, e)
